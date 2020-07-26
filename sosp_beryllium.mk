5a0843f7a1a2a23749d0ee4bffe5814a822daf14
Gnathvm <Gnathvm@gmail.com>
2020-07-26 14:43:58 +0530

    beryllium: Update to build fingerprint from MIUI V11.0.6.0

--- sosp_beryllium.mk
+++ sosp_beryllium.mk
@@ -17,7 +17,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCOPHONE F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/polaris/polaris:8.0.0/OPR1.170623.032/V9.5.19.0.ODGMIFA:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.6.0.QEJMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.6.0.QEJMIXM release-keys" \
