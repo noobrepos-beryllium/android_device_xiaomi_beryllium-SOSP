8082c271938ccb35428f70b7fc9684578ca52918
Gnathvm <Gnathvm@gmail.com>
2020-09-27 09:21:05 +0530

    beryllium : Update fingerprint

--- sosp_beryllium.mk
+++ sosp_beryllium.mk
@@ -17,10 +17,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCOPHONE F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.6.0.QEJMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.0.3.QEJMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.6.0.QEJMIXM release-keys" \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.0.3.QEJMIXM release-keys" \
     PRODUCT_NAME="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
